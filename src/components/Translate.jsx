68a328e04df8bf14586ce9cb560741ce4953ba00
DBH <david@davidbharrison.com>
2022-05-11 09:44:11 -0700

    refactor a bit

--- src/components/Translate.jsx
+++ src/components/Translate.jsx
@@ -19,21 +19,25 @@ const Translate = () => {
         let data = input.split(' ');
         let result = [];
 
-        for (const needle of data) {
-            let found_word = dictionary.find(something => 
-                something.word.toLowerCase() === needle.toLowerCase());
-            let found_code = dictionary.find(something => 
-                something.code.toLowerCase() === needle.toLowerCase());
+        for (const datum of data) {
+            let needle = datum.replace(/[\W_]+/g,'').toLowerCase()
 
-            if (typeof found_word !== 'undefined') {
-                result.push(found_word.code);
+            let found = dictionary.find(entry => 
+                entry.word.toLowerCase() === needle);
+            if (typeof found !== 'undefined') {
+                result.push(found.code);
+                continue;
             }
-            else if (typeof found_code !== 'undefined') {
+            
+            let found_code = dictionary.find(entry => 
+                entry.code.toLowerCase() === needle);            
+            if (typeof found_code !== 'undefined') {
                 result.push(found_code.word);
+                continue;
             }
-            else {
-                result.push(needle);
-            }
+            // if we got here, use what we have w/o translation
+            result.push(needle);
+            
         }
         let result_str = result.join(' ');
         console.log('result_str', result_str);
